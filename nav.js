be4bd7ba024d54f44c71921df622789f9ad36d7b
damyoungtechnologies <damyounggraphics@gmail.com>
2024-05-23 23:40:00 +0100

    Final touch to my code

--- nav.js
+++ nav.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", function(){
     var navbar = document.querySelector(".nav-bar");
     var scrollposition = window.scrollY || document.documentElement.scrollTop;
-    if (scrollposition > 500){
+    if (scrollposition > 470){
         navbar.style.backgroundColor = "rgb(61, 4, 61)";
     }else{
         navbar.style.backgroundColor = "transparent";
@@ -28,9 +28,9 @@ window.addEventListener("scroll", () => {
         }
     }
 
-    setBackgroundColorAndTextColor(about, 500, 940, "rgba(3, 86, 97, 0.8)", "black");
-    setBackgroundColorAndTextColor(experience, 940, 1400, "rgba(3, 86, 97, 0.8)", "black");
-    setBackgroundColorAndTextColor(portfolio, 1400, 1630, "rgba(3, 86, 97, 0.8)", "black");
-    setBackgroundColorAndTextColor(education, 1630, 2000, "rgba(3, 86, 97, 0.8)", "black");
+    setBackgroundColorAndTextColor(about, 470, 750, "rgba(3, 86, 97, 0.8)", "black");
+    setBackgroundColorAndTextColor(experience, 750, 1150, "rgba(3, 86, 97, 0.8)", "black");
+    setBackgroundColorAndTextColor(portfolio, 1150, 1430, "rgba(3, 86, 97, 0.8)", "black");
+    setBackgroundColorAndTextColor(education, 1430, 1900, "rgba(3, 86, 97, 0.8)", "black");
     // setBackgroundColorAndTextColor(connect, 2000, 2100, "white", "black");
 });
